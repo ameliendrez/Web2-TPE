e78a389c1ecaa9f80527c99b7e40a64cdf4ed575
Meliendrez Agustin <ameliendrez@gmail.com>
2017-09-29 17:05:33 -0300

    Comentada correcion en BBDD

--- model/BeerModel.php
+++ model/BeerModel.php
@@ -7,9 +7,9 @@ class BeerModel
 
   function __construct()
   {
-    $this->db = new PDO('mysql:host=localhost;'
-  .'dbname=db_tpe;charset=utf8'
-  , 'root', '');
+  //   $this->db = new PDO('mysql:host=localhost;'
+  // .'dbname=db_tpe;charset=utf8' // Guarda con esto... por que en mi maquina no existe la BBDD db_tpe... habria que ver como crear una auttomaticamente
+  // , 'root', '');
   }
 
   function getCervezas() {
