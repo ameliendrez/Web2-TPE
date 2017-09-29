a5efef8f73e52870c32296551a4bb2b08104ad0d
Meliendrez Agustin <ameliendrez@gmail.com>
2017-09-29 12:03:44 -0300

    Configurado .gitignore para ignorar las carpetas templates_c y cache - Correccion constructor WebBeerController - push de WebBeerModel
    q

--- model/WebBeerModel.php
+++ model/WebBeerModel.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class TareasModel
+class WebBeerModel
 {
   private $db;
 
@@ -12,31 +12,13 @@ class TareasModel
   , 'root', '');
   }
 
-  function getTareas()
-  {
-    $sentencia = $this->db->prepare("select * from tarea");
-    $sentencia->execute();
-    return $sentencia->fetchAll(PDO::FETCH_ASSOC);
-  }
+  // function getTareas()
+  // {
+  //   $sentencia = $this->db->prepare("select * from tarea");
+  //   $sentencia->execute();
+  //   return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+  // }
 
-  function guardarTarea($titulo, $descripcion, $completada)
-  {
-    $sentencia = $this->db->prepare('INSERT INTO tarea(Titulo,Descripcion,Completada) VALUES(?,?,?)');
-    $sentencia->execute([$titulo, $descripcion, $completada]);
-  }
-
-  function borrarTarea($id_tarea)
-  {
-    $sentencia = $this->db->prepare("delete from tarea where IDTarea=?");
-    $sentencia->execute([$id_tarea]);
-  }
-
-  function finalizarTarea($id_tarea)
-  {
-    $sentencia = $this->db->prepare("UPDATE tarea SET Completada=1 where IDTarea=?");
-    $sentencia->execute([$id_tarea]);
-
-  }
 
 
 }
