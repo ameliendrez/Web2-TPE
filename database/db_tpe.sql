cb685f908bfad3d316c732198c5207f8afac586d
santirampoldi <santirampoldi12@gmail.com>
2017-10-05 17:25:43 -0300

    Solucion bug conexion BBDD

--- database/db_tpe.sql
+++ database/db_tpe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-10-2017 a las 23:54:29
+-- Tiempo de generación: 05-10-2017 a las 22:19:39
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
--- model/Model.php
+++ model/Model.php
@@ -1,25 +1,29 @@
 <?php
+
+
+ function buildDDBBfromFile($dbname, $dbfile) {
+  try {
+    $connection = new PDO('mysql:host=localhost', "root", "");
+    $connection->exec('CREATE DATABASE IF NOT EXISTS '.$dbname);
+    $connection->exec('USE '.$dbname);
+    $queries = $this->loadSQLSchema($dbfile);
+    $i = 0;
+    while ($i < count($queries) && strlen($_connection->errorInfo() [2] ) == 0) {
+      $_connection->exec($queries[$i]);
+      echo $queries[$i];
+      $i++;
+    }
+
+  } catch (PDOException $e) {
+    echo $e;
+  }
+
+}
+
   class Model
   {
 
-    public function buildDDBBfromFile($dbname, $dbfile) {
-      try {
-        $this->connection = new PDO('mysql:host=localhost', "root", "");
-        $this->connection->exec('CREATE DATABASE IF NOT EXISTS '.$dbname);
-        $this->connection->exec('USE '.$dbname);
-        $queries = $this->loadSQLSchema($dbfile);
-        $i = 0;
-        while ($i < count($queries) && strlen($this->_connection->errorInfo() [2] ) == 0) {
-          $this->_connection->exec($queries[$i]);
-          echo $queries[$i];
-          $i++;
-        }
 
-      } catch (PDOException $e) {
-        echo $e;
-      }
-
-    }
 
     protected $db;
 
@@ -28,10 +32,10 @@
       try {
 
         $this->db = new PDO('mysql:host=localhost;'
-        .'dbname=db_tpe;charset=utf8'
+        .'dbname=db_tpePRUEBA;charset=utf8'
         , 'root', '');
       } catch (PDOException $e) {
-        buildDDBBfromFile('db_tpe', 'db_tpe.sql');
+        buildDDBBfromFile('db_tpePRUEBA', 'database/db_tpe.sql');
       }
 
     }
