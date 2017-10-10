7c831b72d745a106d7ebdae11d1aad5a497b9544
Meliendrez Agustin <ameliendrez@gmail.com>
2017-10-10 01:28:27 -0300

    modificado SecuredController

---
+++ controller/SecuredController.php
@@ -0,0 +1,27 @@
+<?php
+  include_once 'view/LoginView.php';
+  include_once 'model/LoginModel.php';
+
+  class SecuredController extends Controller
+  {
+    function __construct()
+    {
+      session_start();
+      if(isset($_SESSION['usuario'])){
+
+        if (time() - $_SESSION['LAST_ACTIVITY'] > 5000) {
+        //logout(); // destruye la sesión, y vuelve al login
+        header('Location: '. LOGOUT); // aca utilizamos la redireccion
+        die();
+        }
+
+        $_SESSION['LAST_ACTIVITY'] = time(); // actualiza el último instante de actividad
+      }
+
+      else{
+        header('Location: '. LOGIN);
+        die();
+      }
+    }
+  }
+?>
