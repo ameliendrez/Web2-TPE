98c2d9f8cb956c5c4b8b10a4d45333c83c07a245
santirampoldi <santirampoldi12@gmail.com>
2017-10-11 10:21:36 -0300

    Modificada la estructura de la base de datos para que la vista muestre las ultimas actualizaciones de las tablas

--- model/vista.sql
+++ model/vista.sql
@@ -1,9 +1,9 @@
 select
-`db_tpe`.`cerveza`.`id_cerveza` AS `id_cerveza`
-,`db_tpe`.`cerveza`.`nombre` AS `nombreCerveza`
-,`db_tpe`.`estilocerveza`.`nombre` AS `estilo`
-,`db_tpe`.`cerveza`.`%alc` AS `porcentajeALC`
-,`db_tpe`.`cerveza`.`descripcion` AS `descripcion`
-from `db_tpe`.`cerveza`
-join `db_tpe`.`estilocerveza`
-where (`db_tpe`.`cerveza`.`id_estilo` = `db_tpe`.`estilocerveza`.`id_estilo`)
+`webBeer`.`cerveza`.`id_cerveza` AS `id_cerveza`
+,`webBeer`.`cerveza`.`nombre` AS `nombreCerveza`
+,`webBeer`.`estilocerveza`.`nombre` AS `estilo`
+,`webBeer`.`cerveza`.`%alc` AS `porcentajeALC`
+,`webBeer`.`cerveza`.`descripcion` AS `descripcion`
+from `webBeer`.`cerveza`
+join `webBeer`.`estilocerveza`
+where (`webBeer`.`cerveza`.`id_estilo` = `webBeer`.`estilocerveza`.`id_estilo`)
